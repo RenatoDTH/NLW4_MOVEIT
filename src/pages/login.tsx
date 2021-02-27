dc8a55827e7c1e4cf9f5de294c2d635c0f525286
Renato Castro <renatocastro02@hotmail.com>
2021-02-27 15:16:58 -0300

    feat: add title to login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,30 +1,37 @@
+import Head from 'next/head';
 import styles from '../styles/pages/Login.module.css';
 
 const Login: React.FC = () => {
   return (
-    <div className={styles.container}>
-      <div>
-        <img src="/icons/rectangles.svg" alt="rectangles" />
+    <>
+      <Head>
+        <title>LogIn | move.it</title>
+      </Head>
+
+      <div className={styles.container}>
+        <div>
+          <img src="/icons/rectangles.svg" alt="rectangles" />
+        </div>
+
+        <section className={styles.section}>
+          <img src="/logo.svg" alt="move.it" />
+          <strong>Bem-vindo</strong>
+          <div>
+            <img src="/icons/github.svg" alt="github" />
+
+            <p>Faça login com seu Github para começar</p>
+          </div>
+          <div>
+            <form onSubmit={() => {}}>
+              <input type="text" placeholder="Digite seu username" />
+              <button disabled type="submit">
+                <img src="/icons/right-arrow.svg" alt="entrar" />
+              </button>
+            </form>
+          </div>
+        </section>
       </div>
-
-      <section className={styles.section}>
-        <img src="/logo.svg" alt="move.it" />
-        <strong>Bem-vindo</strong>
-        <div>
-          <img src="/icons/github.svg" alt="github" />
-
-          <p>Faça login com seu Github para começar</p>
-        </div>
-        <div>
-          <form onSubmit={() => {}}>
-            <input type="text" placeholder="Digite seu username" />
-            <button disabled type="submit">
-              <img src="/icons/right-arrow.svg" alt="entrar" />
-            </button>
-          </form>
-        </div>
-      </section>
-    </div>
+    </>
   );
 };
 
